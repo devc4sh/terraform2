c8d1eb37348f9c5b44e6565043164e385da02fae
devc4sh <plra@kakao.com>
2021-12-14 23:20:35 +0900

    +count

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -31,3 +31,17 @@ resource "aws_iam_user" "count" {
 output "count_user_arns" {
   value = aws_iam_user.count.*.arn
 }
+
+resource "aws_iam_user" "for_each_set" {
+  for_each = toset(
+    "for-each-set-user-1"
+    "for-each-set-user-2"
+    "for-each-set-user-3"
+  )
+
+  name = each.key
+}
+
+output "for_each_set_user_arns" {
+  value = values(aws_iam_user.for_each_set).*.arn
+}
